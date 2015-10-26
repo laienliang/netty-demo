c3e3d4c79e3eaf7fa05551a7f7371af2ecc7bce4
laien.liang <laien.liang@vipshop.com>
2015-10-26 14:33:51 +0800

    bio的回显

--- src/main/java/vop/vip/test/bio/BioEchoServer.java
+++ src/main/java/vop/vip/test/bio/BioEchoServer.java
@@ -1,14 +1,14 @@
 package vop.vip.test.bio;
 
-import io.netty.util.CharsetUtil;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
  * 回显功能客户端
@@ -17,14 +17,36 @@ import java.net.Socket;
  *
  */
 public class BioEchoServer {
-	public static void main(String[] args) throws IOException {
-		ServerSocket serverSocket = new ServerSocket(8888);
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(8888);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		int processors = Runtime.getRuntime().availableProcessors();
+		System.out.println(processors);
+		System.out.println("服务启动成功。。。");
+		
+		ThreadFactory tFactory = new ThreadFactoryBuilder().setNameFormat("EchoTaskThread-%d").setDaemon(true).build();
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 4, 2L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(4), tFactory);
+		Socket socket = null;
 		while (!Thread.currentThread().isInterrupted()) {
-			Socket connection = serverSocket.accept();
-			InputStream is = new BufferedInputStream(connection.getInputStream());
-			OutputStream os = new BufferedOutputStream(connection.getOutputStream());
-			os.write("hello".getBytes(CharsetUtil.UTF_8));
-			os.flush();
+			try {
+				socket = serverSocket.accept();
+				System.out.println(String.format("poolsize:%d,taskCount:%d,largestPoolsize:%d, queueSize:%d",executor.getPoolSize(),executor.getTaskCount(), executor.getLargestPoolSize(), executor.getQueue().size()));
+				executor.submit(new EchoTask(socket));
+			} catch (Exception e) {
+				if (socket != null) {
+					try {
+						socket.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+				e.printStackTrace();
+			}
 		}
 	}
 }
