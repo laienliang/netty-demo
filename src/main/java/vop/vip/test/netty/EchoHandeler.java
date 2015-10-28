574e7b8d896a19e9072bb5451cae424e5a5642c2
laien.liang <laien.liang@vipshop.com>
2015-10-28 18:18:32 +0800

    netty服务端

---
+++ src/main/java/vop/vip/test/netty/EchoHandeler.java
@@ -0,0 +1,7 @@
+package vop.vip.test.netty;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoHandeler extends ChannelInboundHandlerAdapter{
+
+}
---
+++ src/main/java/vop/vip/test/netty/NettyEchoServer.java
@@ -0,0 +1,30 @@
+package vop.vip.test.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+
+public class NettyEchoServer {
+	private static final int port = 8888;
+	
+	public static void main(String[] args) throws InterruptedException {
+		NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+		NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+		ServerBootstrap b = new ServerBootstrap().group(bossGroup, workerGroup)
+			.channel(NioServerSocketChannel.class)
+			.childHandler(new ChannelInitializer<SocketChannel>(){
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(new EchoHandeler());
+				}
+
+			});
+		ChannelFuture future = b.bind(port).sync();
+		future.channel().closeFuture().sync();
+	}
+
+}
